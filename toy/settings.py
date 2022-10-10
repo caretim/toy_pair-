746150f9fd7e0e2407f12cd8c7a967e7f992fc1a
caretim <karesin72041@gmail.com>
2022-10-10 22:06:58 +0900

    login

--- toy/settings.py
+++ toy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "users",
     "django_bootstrap5",
     "movies",
     "django.contrib.admin",
@@ -125,3 +126,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
